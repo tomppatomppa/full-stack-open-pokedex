80c5e6ccb851057e940f9f679d96a0316470356d
tomi <tomiwest@hotmail.com>
2023-01-18 10:03:16 +0200

    Update pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
@@ -11,5 +11,5 @@ app.get('/health', (req, res) => {
 })
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 8080')
+  console.log('server started on port 5000')
 })
